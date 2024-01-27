b07fc53eb127580676ba6a46c799f0abf2a59932
camonten <camonten@ucsc.edu>
2024-01-26 16:18:55 -0800

    Added Matlab files and fix environment.yml

---
+++ HyOscillator_Dynamics/OscillatorImpacts.m
@@ -0,0 +1,71 @@
+classdef OscillatorImpacts < HybridSystem
+    % A bouncing ball modeled as a HybridSystem subclass.
+
+    % Define variable properties that can be modified.
+    properties
+        gamma = 9.81;  % Acceleration due to gravity.
+        lambda = 0.6; % Coefficient of restitution.
+        damping = 0.6;
+    end
+
+    % Define constant properties that cannot be modified (i.e., "immutable").
+    properties(SetAccess = immutable) 
+        % The index of 'height' component 
+        % within the state vector 'x'. 
+        height_index = 1;
+        
+        % The index of 'velocity' component 
+        % within the state vector 'x'. 
+        velocity_index = 2;
+    end
+
+    methods 
+        function this = OscillatorImpacts()
+            % Constructor for instances of the BouncingBall class.
+
+            % Call the constructor for the HybridSystem superclass and
+            % pass it the state dimension. This is not strictly necessary, 
+            % but it enables more error checking.
+            state_dim = 2;
+            this = this@HybridSystem(state_dim);
+        end
+
+        % To define the data of the system, we implement 
+        % the abstract functions from HybridSystem.m
+
+        function xdot = flowMap(this, x, t, j)
+            % Extract the state components.
+            v = x(this.velocity_index);
+            h = x(this.height_index);
+
+            % Define the value of f(x). 
+            xdot = [v; -h-this.damping*v];
+        end
+
+        function xplus = jumpMap(this, x)
+            % Extract the state components.
+            v = x(this.velocity_index);
+
+            % Define the value of g(x). 
+            xplus = [0; -this.lambda*v];
+        end
+        
+        function inC = flowSetIndicator(this, x)
+            % Extract the state components.
+            h = x(this.height_index);
+
+            % Set 'inC' to 1 if 'x' is in the flow set and to 0 otherwise.
+%             inC = (h >= 0) || (v >= 0);
+            inC = h >= 0;
+        end
+
+        function inD = jumpSetIndicator(this, x)
+            % Extract the state components.
+            h = x(this.height_index);
+            v = x(this.velocity_index);
+
+            % Set 'inD' to 1 if 'x' is in the jump set and to 0 otherwise.
+            inD = (h <= 0) && (v <= 0);
+        end
+    end
+end
---
+++ HyOscillator_Dynamics/main.m
@@ -0,0 +1,31 @@
+%% Compute solutions to the oscillator with impacts
+
+%--------------------------------------------------------------------------
+% Matlab M-file Project: A Data-Driven Approach for Certifying Asymptotic 
+% Stability and Cost Evaluation for Hybrid Systems @  Hybrid Systems Laboratory (HSL)
+
+% Filename: main.m
+%--------------------------------------------------------------------------
+% Author: Carlos A. Montenegro G.
+%--------------------------------------------------------------------------
+%--------------------------------------------------------------------------
+%   Make sure to install HyEQ Toolbox (Beta) v3.0.0.22 from
+%   https://www.mathworks.com/matlabcentral/fileexchange/41372-hybrid-equations-toolbox
+%   Copyright @ Hybrid Systems Laboratory (HSL),
+
+clear all; clc;
+
+osc_system = OscillatorImpacts();
+x0 = [0.075, 1.1]; % Must lie inside \overline{C} \cap \calU
+tspan = [0, 20];
+jspan = [0, 30];
+config = HybridSolverConfig('refine', 32);
+sol = osc_system.solve(x0, tspan, jspan, config);
+
+% Prepare workspace to export
+x = sol.x;
+ts = sol.t;
+jts = sol.jump_times;
+
+% Export workspace
+save('../data')
